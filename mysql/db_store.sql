41206ec9831484b11ed6737181d214002000c4d6
ilhammhdd <milham939@gmail.com>
2019-06-22 15:18:47 +0700

    added mysql replica server on both prod and dev

---
+++ mysql/db_store.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS kudaki_store DEFAULT COLLATE = utf8_general_ci;
+CREATE USER IF NOT EXISTS 'kudaki_store' @'localhost' IDENTIFIED BY 'kudakistorerocks';
+GRANT ALL PRIVILEGES ON kudaki_store.* TO 'kudaki_store' @'localhost' WITH GRANT OPTION;
+USE kudaki_store;
+CREATE TABLE IF NOT EXISTS storefronts(
+  `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `uuid` VARCHAR(64) NOT NULL UNIQUE,
+  `user_uuid` VARCHAR(64),
+  `total_item` INT(20),
+  `rating` DECIMAL(4, 3),
+  `created_at` BIGINT UNSIGNED
+);
+CREATE TABLE IF NOT EXISTS items(
+  `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `uuid` VARCHAR(64) NOT NULL UNIQUE,
+  `storefront_uuid` VARCHAR(64),
+  `name` VARCHAR(255),
+  `amount` INT(20),
+  `unit` VARCHAR(255),
+  `price` INT(20),
+  `description` TEXT,
+  `photo` VARCHAR(255),
+  `rating` DECIMAL(4, 3),
+  `created_at` BIGINT UNSIGNED,
+  FOREIGN KEY(storefront_uuid) REFERENCES storefronts(uuid) ON DELETE CASCADE
+);
